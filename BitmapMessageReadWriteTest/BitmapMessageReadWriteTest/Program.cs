5a7696e9fa6ff985cda1cdffca3c065b5b21243d
svisstack <svisstack@gmail.com>
2014-12-11 14:41:36 +0100

    hiding, reading messages from bmp

---
+++ BitmapMessageReadWriteTest/BitmapMessageReadWriteTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+using MessageSteganography;
+
+namespace BitmapMessageReadWriteTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[1] read test.bmp, hide message, save to test2.bmp");
+            Console.WriteLine("[2] read test.bmp, decode message, read test2.bmp, decode message");
+
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.D1)
+            {
+                Bitmap bmp = new Bitmap("test.bmp");
+
+                Console.WriteLine("MESSAGE in test.bmp: {0}.",
+                    MessageSteganography.MessageSteganography.readMessage(bmp));
+
+                string message = Guid.NewGuid().ToString();
+                Console.WriteLine("Writing message {0} to test2.bmp.", message);
+                MessageSteganography.MessageSteganography.hideMessage(bmp, message);
+
+                bmp.Save("test2.bmp");
+
+                Console.WriteLine("ReadKey();");
+                Console.ReadKey();
+            }
+            else if (key.Key == ConsoleKey.D2)
+            {
+                Bitmap bmp = new Bitmap("test.bmp");
+
+                Console.WriteLine("MESSAGE in test.bmp: {0}.",
+                    MessageSteganography.MessageSteganography.readMessage(bmp));
+
+                Bitmap bmp2 = new Bitmap("test2.bmp");
+
+                Console.WriteLine("MESSAGE in test2.bmp: {0}.",
+                    MessageSteganography.MessageSteganography.readMessage(bmp2));
+
+                Console.WriteLine("ReadKey();");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ MessageSteganography/MessageSteganography.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MessageSteganography
+{
+    public static class MessageSteganography
+    {
+        private static string MAGIC = "{FC6EA6D1-3690-483D-A662-D0BA9D79493E}";
+
+        public static void hideMessage(Bitmap bmp, string message)
+        {
+            // Lock the bitmap's bits.  
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            System.Drawing.Imaging.BitmapData bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Declare an array to hold the bytes of the bitmap. 
+            int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            // Copy the RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            rgbValues = hideMessage(rgbValues, message);
+
+            // Copy the RGB values into the bitamp.
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            bmp.UnlockBits(bmpData);
+        }
+        public static string readMessage(Bitmap bmp)
+        {            
+            // Lock the bitmap's bits.  
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            System.Drawing.Imaging.BitmapData bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Declare an array to hold the bytes of the bitmap. 
+            int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            // Copy the RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            bmp.UnlockBits(bmpData);
+
+            return readMessage(rgbValues);
+
+        }
+
+        private static byte[] hideMessage(byte[] data, string message)
+        {
+            message = MAGIC + message;
+            string bitsMessage = GetBits(message) + "00000000";
+
+            if (data.Length < bitsMessage.Length)
+            {
+                throw new InvalidOperationException("not enough space.");
+            }
+
+            byte[] dataOut = (byte[])data.Clone();
+            for (int idx = 0; idx < bitsMessage.Length; idx++)
+            {
+                if (bitsMessage[idx] == '0')
+                {
+                    dataOut[idx] &= 0xFE;
+                }
+                else if (bitsMessage[idx] == '1')
+                {
+                    dataOut[idx] |= 0x01;
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+
+            return dataOut;
+        }
+
+        private static string readMessage(byte[] data)
+        {
+            string bitsMessage = "";
+            int lastZeros = 0;
+
+            for (int idx = 0; idx < data.Length; idx++)
+            {
+                if ((data[idx] & (byte)0x01) > 0)
+                {
+                    bitsMessage += "1";
+                    lastZeros = 0;
+                }
+                else
+                {
+                    bitsMessage += "0";
+                    lastZeros++;
+                }
+
+                if (lastZeros >= 8)
+                {
+                    bitsMessage = bitsMessage.Remove(bitsMessage.Length - 8, 8);
+                    break;
+                }
+            }
+
+            //int idxEOF = bitsMessage.IndexOf("00000000");
+            //if (idxEOF >= 0)
+            //{
+            //    bitsMessage = bitsMessage.Remove(idxEOF, bitsMessage.Length - idxEOF);
+            //}
+
+            long result;
+            Math.DivRem(bitsMessage.Length, 8, out result);
+            if (result > 0)
+            {
+                bitsMessage = bitsMessage.Remove(bitsMessage.Length - (int)result, (int)result);
+            }
+
+            byte[] byteString = new byte[bitsMessage.Length / 8];
+            for (int idx = 0; idx < byteString.Length; idx++)
+            {
+                byte b = 0;
+                for (int jdx = 0; jdx < 8; jdx++)
+                {
+                    if (bitsMessage[(idx * 8) + jdx] == '1')
+                    {
+                        b |= (byte)(0x80 >> jdx);
+                    }
+                }
+                byteString[idx] = b;
+            }
+
+            string message = System.Text.Encoding.ASCII.GetString(byteString);
+            if (message.StartsWith(MAGIC))
+            {
+                return message.Remove(0, MAGIC.Length);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private static string GetBits(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in Encoding.ASCII.GetBytes(input))
+            {
+                string bits = Convert.ToString(b, 2);
+                while (bits.Length < 8)
+                {
+                    bits = "0" + bits;
+                }
+                sb.Append(bits);
+            }
+            return sb.ToString();
+        }
+    }
+}
